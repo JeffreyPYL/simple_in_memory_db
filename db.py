8bd9f21233198c4ef6fe289f0da0e8223d46519f
Jeffrey Leung <jeffrey.pyl@outlook.com>
2020-07-25 16:39:24 -0400

    Basic function without transaction

---
+++ db.py
@@ -0,0 +1,93 @@
+'''
+Your API should accept the following:
+
+● void createTransaction(String transactionId)
+    ○ Starts a transaction with the specified ID. The ID must not be an active
+    transaction ID.
+    ○ Throws an exception or returns an error on failure
+● void rollbackTransaction(String transactionId)
+    ○ Aborts the transaction and invalidates the transaction with the specified
+    transaction ID
+    ○ Throws an exception or returns an error on failure
+● void commitTransaction(String transactionId)
+    ○ Commits the transaction and invalidates the ID. If there is a conflict (meaning the
+    transaction attempts to change a value for a key that was mutated after the
+    transaction was created), the transaction always fails with an exception or an
+    error is returned.
+    Transaction should be isolated at the read committed level, as defined by this Wikipedia page.
+    Any put, delete, get operation that is issued without a transaction ID should commit immediately.
+'''
+
+
+class SimpleDB(object):
+    def __init__(self):
+        # In memory JSON Object
+        self.db = {}
+
+    '''
+    ● void put(String key, String value)
+        ○ Set the variable “key” to the provided “value”
+        ○ Throws an exception or returns an error on failure
+    ● void put(String key, String value, String transactionId)
+        ○ Set the variable “key” to the provided “value” within the transaction with ID
+        “transactionId”
+        ○ Throws an exception or returns an error on failure
+    '''
+    def put(self, key: str, value: str, transactionId: str):
+        try:
+            self.db[str(key)] = value
+        except Exception as error:
+            raise error
+
+    '''
+    ● String get(String key)
+        ○ Returns the value associated with “key”
+        ○ Throws an exception or returns an error on failure
+    ● String get(String key, String transactionId)
+        ○ Returns the value associated with “key” within the transaction with ID
+        “transactionId”
+        ○ Throws an exception or returns an error on failure
+    '''
+    def get(self, key: str, transactionId: str):
+        try:
+            return self.db[key]
+        except Exception as error:
+            raise error
+
+    '''
+    ● void delete(String key)
+        ○ Remove the value associated with “key”
+        ○ Throws an exception or returns an error on failure
+    ● void delete(String key, String transactionId)
+        ○ Remove the value associated with “key” within the transaction with ID
+        “transactionId”
+        ○ Throws an exception or returns an error on failure
+    '''
+    def delete(self, key: str, transactionId: str):
+         
+        if not key in self.db:
+            raise Exception("Error, key not in db")
+        try:
+            del self.db[key]
+        except Exception as error:
+            raise error
+
+    '''
+    Your API should accept the following:
+
+    ● void createTransaction(String transactionId)
+        ○ Starts a transaction with the specified ID. The ID must not be an active
+        transaction ID.
+        ○ Throws an exception or returns an error on failure
+    ● void rollbackTransaction(String transactionId)
+        ○ Aborts the transaction and invalidates the transaction with the specified
+        transaction ID
+        ○ Throws an exception or returns an error on failure
+    ● void commitTransaction(String transactionId)
+        ○ Commits the transaction and invalidates the ID. If there is a conflict (meaning the
+        transaction attempts to change a value for a key that was mutated after the
+        transaction was created), the transaction always fails with an exception or an
+        error is returned.
+        Transaction should be isolated at the read committed level, as defined by this Wikipedia page.
+        Any put, delete, get operation that is issued without a transaction ID should commit immediately.
+    '''
